734ec1070219032059759844cfff00a5793b31e6
YeMyintSoeKMD <yms.yemyintsoe.kmd@gmail.com>
2024-02-22 18:43:43 +0630

    yms

--- src/stores/skill.js
+++ src/stores/skill.js
@@ -1,35 +1,36 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue';
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useSkillStore = defineStore('skill', () => {
-    const skillCategories = ref([
-        {
-            id: 1,
-            name: 'Front-End Development',
-        },
-        {
-            id: 2,
-            name: 'Back-End Development',
-        }
-    ]);
+export const useSkillStore = defineStore("skill", () => {
+  const skillCategories = ref([
+    {
+      id: 1,
+      name: "Front-End Development",
+    },
+    {
+      id: 2,
+      name: "Back-End Development",
+    },
+  ]);
 
-    const skills = ref([
-        {
-            id: 1,
-            category: 'Front-End Development',
-            content: 'HTML, CSS, JavaScript, Bootstrap, TailwindCSS, Materialize CSS',
-        },
-        {
-            id: 2,
-            category: 'Front-End Development',
-            content: 'jQuery, Vue.js (Vue, Pinia, Vue-Router), React.js, TypeScrip',
-        },
-        {
-            id: 3,
-            category: 'Back-End Development',
-            content: 'PHP (MySQLi, PDO, OOP, MVC), Laravel Framework, SQL, MySQL, PostgreSQL'
-        }
-    ]);
+  const skills = ref([
+    {
+      id: 1,
+      category: "Front-End Development",
+      content: "HTML, CSS, JavaScript, Bootstrap, TailwindCSS, Materialize CSS",
+    },
+    {
+      id: 2,
+      category: "Front-End Development",
+      content: "jQuery, Vue.js (Vue, Pinia, Vue-Router), React.js, TypeScrip",
+    },
+    {
+      id: 3,
+      category: "Back-End Development",
+      content:
+        "PHP (MySQLi, PDO, OOP, MVC), Laravel Framework, SQL, MySQL, PostgreSQL",
+    },
+  ]);
 
-    return { skillCategories, skills };
-})
+  return { skillCategories, skills };
+});
